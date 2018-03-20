14188b7895d3d57acd3213b8bc77f2314ab3e029
dmoldova <35409629+dmoldova@users.noreply.github.com>
2018-03-20 16:57:35 -0700

    Update vm_subscriber.py

--- ee250/lab07/vm_subscriber.py
+++ ee250/lab07/vm_subscriber.py
@@ -5,16 +5,30 @@ Run vm_subscriber.py in a separate terminal on your VM."""
 import paho.mqtt.client as mqtt
 import time
 
+ultrasonic = " "
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
-
+    
+    client.subscribe("anrg-pi6/unltrasonicRanger")
+    client.message_callback_add("anrg-pi6/ultrasonicRanger", custom_callback_ultrasonic)
+                                
+    client.subscribe("anrg-pi6/button")
+    client.message_callback_add("anrg-pi6/button", custom_callback_button)
+                                
     #subscribe to the ultrasonic ranger topic here
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
     print("on_message: " + msg.topic + " " + str(msg.payload))
-
+                                
+def custom_callback_ultrasonic(client, userdata, message):
+    ultrasonic = str(message.payload)
+    print("custom_callback_ultrasonic: " + message.topic + " " + str(message.payload))
+    print("custom_callback_ultrasonic: message.payload is type " + str(type(message.payload)))$
+                                
+def custom_callback_button(client, userdata, message):
+    print(str(message.payload))
 
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
