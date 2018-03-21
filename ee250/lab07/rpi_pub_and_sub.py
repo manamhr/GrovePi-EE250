6c5d74c7c00404df8b0f0733fd62103703c0759f
Mana <pi@anrg-pi6.lan>
2018-03-21 00:59:15 +0000

    last changes

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -15,34 +15,35 @@ led=3
 button=5
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
-    grovepi.pinMode(button, "Press a button")
-    client.subscribe("anrg-pi6/led")
-    client.msg_callback("aneg-pi6/led", callback_led)
-    client.subscribe("anrg-pi6/lcd")
-    client.msg_callback("anrg-pi6/lcd", callback_lcd)
+	print("Connected to server (i.e., broker) with result code "+str(rc))
+	grovepi.pinMode(button, "Press a button")
+	client.subscribe("anrg-pi6/led")
+	client.msg_callback("aneg-pi6/led", callback_led)
+	client.subscribe("anrg-pi6/lcd")
+	client.msg_callback("anrg-pi6/lcd", callback_lcd)
     #subscribe to topics of interest here
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
-    print("on_message: " + msg.topic + " " + str(msg.payload))
+	print("on_message: " + msg.topic + " " + str(msg.payload))
 
 def callback_led(client,userdata, msg):
 	if ("LED_ON" in str(msg.payload)):
 		try:
 			digitalWrite(led,1)
-	        print("callback_led:"+msg.topic+" " +std(msg.payload))
-        	print("callback_led:msg.payload  has a type of : " + str(type(msg.payload)))
-
+			print("callback_led:"+msg.topic+" " +std(msg.payload))
+			print("callback_led:msg.payload  has a type of : " + str(type(msg.payload)))
 	elif("LED_OFF" in str(msg.payload)):
-		try: 
+		try:
 			digitalWrite(led,0)
-	        print("callback_led:" +msg.topic+" " +str(msg.payload))
-        	print ("callback_led: msg.payload has a type of : " + str(type(msg.payload)))
+			print("callback_led:" +msg.topic+" " +str(msg.payload))
+			print ("callback_led: msg.payload has a type of : " + str(type(msg.payload)))
 
 def callback_lcd(client, userdata,msg):
 	setText(str(msg.payload))
-def on_press(key):
+
+
+
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
