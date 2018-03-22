e4588fb1afe60e08654453b0310ca5048479b726
dmoldova <35409629+dmoldova@users.noreply.github.com>
2018-03-22 16:35:28 -0700

    Update vm_publisher.py

--- ee250/lab07/vm_publisher.py
+++ ee250/lab07/vm_publisher.py
@@ -7,55 +7,52 @@ import time
 from pynput import keyboard
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
+	print("Connected to server (i.e., broker) with result code "+str(rc))
 
     #subscribe to topics of interest here
 
 #Default message callback. Please use custom callbacks.
 def on_message(client, userdata, msg):
-    print("on_message: " + msg.topic + " " + str(msg.payload))
+	print("on_message: " + msg.topic + " " + str(msg.payload))
 
 def on_press(key):
-    try: 
-        k = key.char # single-char keys
-    except: 
-        k = key.name # other keys
-    
-    if k == 'w':
-        print("w")
-client.publish("anrg-pi6/lcd","w")
-        #send "w" character to rpi
-    elif k == 'a':
-        print("a")
-	client.publish("anrg-pi6/lcd","a")
-	client.publish("anrg-pi6/led","LED_ON")
-        # send "a" character to rpi
-        #send "LED_ON"
-    elif k == 's':
-        print("s")
-	client.publish("anrg-pi6/lcd","s")
-        # send "s" character to rpi
-    elif k == 'd':
-        print("d")
-	client.publish("anrg-pi6/lcd","d")
-	client.publish("anrg-pi6/led","LED_OFF")
-        # send "d" character to rpi
-        # send "LED_OFF"
+	try:
+		k = key.char # single-char keys
+	except: 
+		k = key.name # other keys
+		
+	if k == 'w':
+		print("w")
+		client.publish("anrg-pi6/lcd", "w")
+		
+	elif k == 'a':
+		print("a")
+		client.publish("anrg-pi6/lcd","a")
+		client.publish("anrg-pi6/led","LED_ON")
+		
+	elif k == 's':
+		print("s")
+		client.publish("anrg-pi6/lcd","s")
+		
+	elif k == 'd':
+		print("d")
+		client.publish("anrg-pi6/lcd","d")
+		client.publish("anrg-pi6/led","LED_OFF")
 
 if __name__ == '__main__':
     #setup the keyboard event listener
-    lis = keyboard.Listener(on_press=on_press)
-    lis.start() # start to listen on a separate thread
+	lis = keyboard.Listener(on_press=on_press)
+	lis.start() # start to listen on a separate thread
 
     #this section is covered in publisher_and_subscriber_example.py
-    client = mqtt.Client()
-    client.on_message = on_message
-    client.on_connect = on_connect
-    client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
-    client.loop_start()
+	client = mqtt.Client()
+	client.on_message = on_message
+	client.on_connect = on_connect
+	client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
+	client.loop_start()
 
-    while True:
-        print("delete this line")
-        time.sleep(1)
+	while True:
+		print("delete this line")
+		time.sleep(1)
             
 
