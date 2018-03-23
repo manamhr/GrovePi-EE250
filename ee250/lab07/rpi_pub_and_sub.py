4c89ab67c7be002595a33c1ca3ad34a27aae568c
dmoldova <35409629+dmoldova@users.noreply.github.com>
2018-03-23 15:22:03 -0700

    Update rpi_pub_and_sub.py

--- ee250/lab07/rpi_pub_and_sub.py
+++ ee250/lab07/rpi_pub_and_sub.py
@@ -22,9 +22,9 @@ def on_connect(client, userdata, flags, rc):
 	print("Connected to server/broker with result code "+str(rc))
 	grovepi.pinMode(button, "INPUT") #button is an input
 	client.subscribe("anrg-pi6/led")
-	#client.msg_callback("anrg-pi6/led", callback_led)
+	client.message_callback_add("anrg-pi6/led", callback_led)
 	client.subscribe("anrg-pi6/lcd")
-	#client.msg_callback("anrg-pi6/lcd", callback_lcd)
+	client.message_callback_add("anrg-pi6/lcd", callback_lcd)
     #subscribe to topics of interest here
 
 #Default message callback. Please use custom callbacks.
