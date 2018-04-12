5e9dc6f7474299ed73c4d800e230ceff87c4f467
Mana <mehraein@usc.edu>
2018-04-11 18:04:05 -0700

    lab9 changes

--- ee250/lab07/vm_publisher.py
+++ ee250/lab07/vm_publisher.py
@@ -5,6 +5,10 @@ Run vm_publisher.py in a separate terminal on your VM."""
 import paho.mqtt.client as mqtt
 import time
 from pynput import keyboard
+from grovepi import *
+from grove_rgb_lcd import *
+
+dht_sensor_port = 7
 
 def on_connect(client, userdata, flags, rc):
 	print("Connected to server (i.e., broker) with result code "+str(rc))
@@ -19,30 +23,11 @@ def callback_led(client, userdata, msg):
 	print("callback_led:" + msg.topic + " " + str(msg.payload, "utf-8"))
 	print("callback_led: msg.payload has a type of : " + str(type(msg.payload, "utf-8")))
 
-def on_press(key):
-	try:
-		k = key.char # single-char keys
-	except: 
-		k = key.name # other keys
+def on_press_LED(LED_toggle):
+	
+		client.publish("anrg-pi6/led", "LED_toggle")
 		
-	if k == 'w':
-		print("w")
-		client.publish("anrg-pi6/lcd", "w")
-		
-	elif k == 'a':
-		print("a")
-		client.publish("anrg-pi6/lcd","a")
-		client.publish("anrg-pi6/led","LED_ON")
-		
-	elif k == 's':
-		print("s")
-		client.publish("anrg-pi6/lcd","s")
-		
-	elif k == 'd':
-		print("d")
-		client.publish("anrg-pi6/lcd","d")
-		client.publish("anrg-pi6/led","LED_OFF")
-
+# do sth for messages
 if __name__ == '__main__':
     #setup the keyboard event listener
 	lis = keyboard.Listener(on_press=on_press)
@@ -57,6 +42,25 @@ if __name__ == '__main__':
 
 	while True:
 		#print("delete this line")
+		try:
+			[temp, hum] = dht(dht_sensor+port,1) #get temp and hum
+			#print "temp=", temp, "C\thumadity =", hum, "%" 
+			client.publish("anrg-pi6/temperature", temp)
+			client.publish("anrg-pi6/humidity", hum)
+			t = str(temp)
+			h = str(hum)
+
+			setRGB(0,128,64)
+			setRGB(0,255,0)
+			setText("Temp:" +t+ "C      " + "Humidity: "  + h + "%")
+
+		except (IOError, TypeError) as e:
+			print "Error"
+
+
+
+
+
 		time.sleep(1)
             
 
