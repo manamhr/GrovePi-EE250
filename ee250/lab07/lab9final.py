c376b1af142ca2736e85f6a9f46ad1a4c60fc4c2
dmoldova <35409629+dmoldova@users.noreply.github.com>
2018-04-17 17:42:25 -0700

    Update lab9final.py

--- ee250/lab07/lab9final.py
+++ ee250/lab07/lab9final.py
@@ -4,11 +4,11 @@ import grovepi
 from grovepi import *
 from grove_rgb_lcd import *
 
-global led, dhtPIN
-led = 3
-dhtPIN = 7
+global led, dhtPIN #globes
+led = 3 #the led
+dhtPIN = 7 #the humidity sensor
 
-grovepi.pinMode(led, "OUTPUT")
+grovepi.pinMode(led, "OUTPUT") #declaring global pinmode for the LED
 
 def led_callback(client, userdata, message):
 	if (str(message.payload, "utf-8") == "LED_toggle"):
@@ -23,13 +23,15 @@ def lcd_callback(client, userdata, message):
 def on_connect(client, userdata, flags, rc):
 	print("Connected to server (i.e., broker) with result code "+str(rc))
 
+	#subscribe LED
 	client.subscribe("anrg-pi6/led")
 	client.message_callback_add("anrg-pi6/led", led_callback)
-	print("Connected to LED")
+	#print("Connected to LED") #for debugging
 
+	#subscribe LCD
 	client.subscribe("anrg-pi6/lcd")
 	client.message_callback_add("anrg-pi6/lcd", lcd_callback)
-	print("Connected to LCD")
+	#print("Connected to LCD")	#for debugging
 
 def on_message(client, userdata, msg):
 	print("on_message: " + msg.topic + " " + str(msg.payload))
@@ -40,13 +42,14 @@ if __name__ == '__main__':
 	client.on_connect = on_connect
 	client.connect(host = "eclipse.usc.edu", port = 11000, keepalive = 60)
 	client.loop_start()
-	pinMode(led, "OUTPUT")
+	pinMode(led, "OUTPUT") #making sure it's an output
 	time.sleep(1)
-	setRGB(0,0,255)
+	setRGB(0,0,255) #set screen color
 
 	while True:
 		[temp, hum] = dht(dhtPIN, 1)
 
+		#publish results to app
 		client.publish("anrg-pi6/temperature", str(temp))
 		client.publish("anrg-pi6/humidity", str(hum))
 		time.sleep(1)
